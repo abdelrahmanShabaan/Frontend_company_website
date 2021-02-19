0bc8f2b4cd66964173f7aa4d4793929c4f5560be
boodyelmasry6@gmail.com <boodyelmasry6@gmail.com>
2021-02-19 03:29:22 +0200

    website company

---
+++ js/custom.js
@@ -0,0 +1,85 @@
+// global $ , alert ,console 
+
+$(function () {
+
+		'use strict';
+
+		//adjust header height
+		var myHeader = $('.header'),
+		mySlider = $('.bxslider');
+
+			myHeader.height($(window).height());
+		$(window).resize(function() {
+			myHeader.height($(window).height());
+
+					// adjust bx slider list items center
+				mySlider.each(function () {
+					$(this).css('paddingTop',($(window).height() - $('.bxslider li').height())/2);
+				});
+
+
+		});
+
+		//links active class 
+		$('.links li a').click(function() {
+			$(this).parent().addClass('active').siblings().removeClass('active');
+		});
+
+		// adjust bx slider list items center
+		mySlider.each(function () {
+				$(this).css('paddingTop',($(window).height() - $('.bxslider li').height())/2);
+			});
+
+
+		// TRIGGER THE BX SLIDER
+		mySlider.bxSlider({
+			pager : false 
+		});
+
+		// Smoth Scroll TO Div
+		$('.links li a').click(function(){	
+			$('html , body').animate({
+				scrollTop : $('#' + $(this).data('value')).offset().top
+			},1000);
+
+			console.log('#' + $(this).date('value'));
+
+		});
+
+
+		// Our Auto Slider Code
+		(function autoSlider() {
+			$('.slider .active').each(function () {
+				if(!$(this).is(':last-child')) {
+					$(this).delay(3000).fadeOut(1000, function () {
+					 $(this).removeClass('active').next().addClass('active').fadeIn();
+					 autoSlider();
+					});
+				}else {
+					$(this).delay(3000).fadeOut(1000 , function () {
+						$(this).removeClass('active');
+						$('.slider div').eq(0).addClass('active').fadeIn();
+						autoSlider();
+					});
+				}
+			});
+
+		}());
+
+		// Triger Mixit up
+		$('#Container').mixItUp();
+
+		// Adjust shuffle links
+		$('.shuffle li').click(function () {
+			$(this).addClass('selected').siblings().removeClass();
+		});
+
+		// Triger Nice Scroll
+		    $('body').niceScroll({
+
+		    	cursorcolor: "#1abc9c"
+		    });
+
+
+
+});
